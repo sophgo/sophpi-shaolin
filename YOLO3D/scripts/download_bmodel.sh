1e95a1c29621381b322b7a1d5e2f5ff2b19fc3c9
zwysophon <wangyang.zuo@sophgo.com>
2022-11-08 15:19:01 +0800

    fix(YOLO3D): fix download_bmodel.sh bug

--- YOLO3D/scripts/download_bmodel.sh
+++ YOLO3D/scripts/download_bmodel.sh
@@ -1,10 +1,12 @@
 pip3 install dfn
 
-python3 -m dfn http://219.142.246.77:65000/sharing/pu4tB1COL
+python3 -m dfn --url http://219.142.246.77:65000/sharing/pu4tB1COL
+
+mkdir ../data/models
 
 mv ./resnet18.bmodel ../data/models/resnet18.bmodel
 
-python3 -m dfn http://219.142.246.77:65000/sharing/gZn6rY08P
+python3 -m dfn --url http://219.142.246.77:65000/sharing/gZn6rY08P
 
 mv ./yolov5s.bmodel ../data/models/yolov5s.bmodel
 
